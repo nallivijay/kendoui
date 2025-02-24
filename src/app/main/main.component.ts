e318db9276e474db39ee80636be201fe913606bf
Sherisaiakshayreddy11 <Sherisaiakshayreddy@gmail.com>
2025-02-24 17:36:28 +0530

    latest

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -2,24 +2,21 @@ import { Component } from '@angular/core';
 import { KENDO_DROPDOWNS } from '@progress/kendo-angular-dropdowns';
 import { KENDO_DATEINPUTS } from '@progress/kendo-angular-dateinputs';
 import { GridComponent } from "../grid/grid.component";
-
+import { calendarIcon, chevronDownIcon } from '@progress/kendo-svg-icons';
+import { KENDO_ICONS } from '@progress/kendo-angular-icons';
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [KENDO_DROPDOWNS, KENDO_DATEINPUTS, GridComponent],
+  imports: [KENDO_DROPDOWNS, KENDO_DATEINPUTS, GridComponent,KENDO_ICONS],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
 export class MainComponent {
+  public dateic = calendarIcon;
+  public chevron = chevronDownIcon;
   public listItems: Array<string> = [
-    "Baseball",
-    "Basketball",
-    "Cricket",
-    "Field Hockey",
-    "Football",
-    "Table Tennis",
-    "Tennis",
-    "Volleyball",
+    "Approved",
+    "Rejected"
   ];
 
   public range = {
