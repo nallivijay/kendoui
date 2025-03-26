7628f9f233243b594129e4b95f5d41ddf60631f1
Sherisaiakshayreddy11 <Sherisaiakshayreddy@gmail.com>
2025-03-26 17:40:41 +0530

    26-03-2025

--- src/app/design-system/design-system.component.ts
+++ src/app/design-system/design-system.component.ts
@@ -1,26 +1,29 @@
 import { Component } from '@angular/core';
 import { KENDO_DROPDOWNS } from '@progress/kendo-angular-dropdowns';
 import { KENDO_ICONS } from '@progress/kendo-angular-icons';
-import { bookIcon, checkIcon, chevronDownIcon, eyeIcon, fileAddIcon, paperclipIcon, SVGIcon, userIcon } from '@progress/kendo-svg-icons';
+import { bookIcon, calendarIcon, checkIcon, chevronDownIcon, eyeIcon, fileAddIcon, paperclipIcon, SVGIcon, userIcon } from '@progress/kendo-svg-icons';
 import { KENDO_INPUTS } from '@progress/kendo-angular-inputs';
 import { KENDO_LABEL } from '@progress/kendo-angular-label';
 import { KENDO_BUTTONS } from '@progress/kendo-angular-buttons';
 import { KENDO_LAYOUT } from '@progress/kendo-angular-layout';
 import { KENDO_GRID } from '@progress/kendo-angular-grid';
+import { KENDO_DATEINPUTS } from '@progress/kendo-angular-dateinputs';
 
 import { FormsModule } from '@angular/forms';
 import { Product } from './model';
 
+
 @Component({
   selector: 'app-design-system',
   standalone: true,
-  imports: [KENDO_DROPDOWNS,KENDO_ICONS,KENDO_INPUTS,KENDO_LABEL,KENDO_BUTTONS,KENDO_LAYOUT,KENDO_GRID,FormsModule],
+  imports: [KENDO_DROPDOWNS,KENDO_ICONS,KENDO_INPUTS,KENDO_LABEL,KENDO_BUTTONS,KENDO_LAYOUT,KENDO_GRID,FormsModule,KENDO_DATEINPUTS],
   templateUrl: './design-system.component.html',
   styleUrl: './design-system.component.scss'
 })
 export class DesignSystemComponent {
   public checkIcon = checkIcon;
   public chevron = chevronDownIcon;
+  public dateic = calendarIcon;
   public listItems: Array<string> = [
     "Approved",
     "Rejected"
@@ -57,7 +60,11 @@ export class DesignSystemComponent {
     { label: "Step Title", isValid: true},
   ];
 
-
+  
+  public range = {
+    start: new Date(2025, 6, 3),
+    end: new Date(2025, 6, 9),
+  };
 
 
 
